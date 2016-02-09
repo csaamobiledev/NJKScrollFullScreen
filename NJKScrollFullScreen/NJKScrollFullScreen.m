45896bc781f03402562d00f0304e42fa8d7b0eeb
Mobile Dev Team <csaa.mobile.dev@gmail.com>
2016-02-09 11:31:30 -0700

    Update NJKScrollFullScreen.m
    update the nav bar even on bouncing - Forked the repo to test the change.

--- NJKScrollFullScreen/NJKScrollFullScreen.m
+++ NJKScrollFullScreen/NJKScrollFullScreen.m
@@ -62,10 +62,10 @@ NJKScrollDirection detectScrollDirection(currentOffsetY, previousOffsetY)
     BOOL isOverTopBoundary = currentOffsetY <= topBoundary;
     BOOL isOverBottomBoundary = currentOffsetY >= bottomBoundary;
 
-    BOOL isBouncing = (isOverTopBoundary && currentScrollDirection != NJKScrollDirectionDown) || (isOverBottomBoundary && currentScrollDirection != NJKScrollDirectionUp);
-    if (isBouncing || !scrollView.isDragging) {
-        return;
-    }
+    // BOOL isBouncing = (isOverTopBoundary && currentScrollDirection != NJKScrollDirectionDown) || (isOverBottomBoundary && currentScrollDirection != NJKScrollDirectionUp);
+    // if (isBouncing || !scrollView.isDragging) {
+    //     return;
+    // }
 
     CGFloat deltaY = _previousOffsetY - currentOffsetY;
     _accumulatedY += deltaY;
